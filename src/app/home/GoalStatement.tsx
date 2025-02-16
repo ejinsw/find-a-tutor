273f7bf6f039965e54ebb28a416ccfe83137ba30
Hannie <yuhan.xie04@gmail.com>
2025-02-15 18:50:29 -0800

    updated GoalStatement component

--- src/app/home/GoalStatement.tsx
+++ src/app/home/GoalStatement.tsx
@@ -7,5 +7,13 @@ interface Props {
 
 export function GoalStatement({ className }: Props) {
   // TODO: Finish Goal Statement Section
-  return <div className={`${className}`}></div>;
+
+  return (
+    <div className={`${className} p-8 text-center`}>
+      <h2 className="text-3xl font-semibold">Our Goal</h2>
+      <p className="mt-4 text-lg text-gray-700 max-w-2xl mx-auto">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  );
 }

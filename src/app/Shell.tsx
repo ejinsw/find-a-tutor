14146c5f093d8ea6896f5658a00de96e994aa840
Dae Hoon Chung <daehoon.chung8@gmail.com>
2025-02-16 13:58:28 -0800

    Adding new pfps and appropriate requirements

--- src/app/Shell.tsx
+++ src/app/Shell.tsx
@@ -3,6 +3,7 @@
 import { AppShell, Burger, Center, Group, UnstyledButton } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import Link from "next/link";
+import { LoginButton } from "./LoginButton";
 
 interface Props {
   children?: React.ReactNode;
@@ -10,10 +11,11 @@ interface Props {
 
 export function Shell({ children }: Props) {
   const [opened, { toggle }] = useDisclosure();
+  const user = null;
 
   return (
     <AppShell
-      header={{ height: 60 }}
+      header={{ height: 96 }}
       navbar={{
         width: 300,
         breakpoint: "sm",
@@ -22,31 +24,35 @@ export function Shell({ children }: Props) {
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Group justify="space-between" style={{ flex: 1 }}>
-            <div>Logo</div>
-            <Group visibleFrom="sm">
-              <UnstyledButton component={Link} href="/home" className={""}>
-                Home
-              </UnstyledButton>
-              <UnstyledButton component={Link} href="/contact" className={""}>
-                Contact
-              </UnstyledButton>
-              <UnstyledButton component={Link} href="/about" className={""}>
-                About
-              </UnstyledButton>
-            </Group>
+        <Group
+          justify="space-between"
+          align="center"
+          className="px-4 h-full w-full"
+        >
+          <Link
+            href="/"
+            className="tracking-tighter uppercase text-xl font-extrabold font-sans text-black hover:scale-[101%] transition-transform duration-100 ease-out"
+          >
+            Find-a-Tutor
+          </Link>
+
+          <Group gap="md" className="h-full" visibleFrom="md">
+            <Link href="/home">Home</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/about">About</Link>
+            {!user && <LoginButton />}
           </Group>
+
+          <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="sm" />
         </Group>
       </AppShell.Header>
 
-      <AppShell.Navbar className="flex flex-col">
+      <AppShell.Navbar className="flex flex-col" hiddenFrom="md">
         <UnstyledButton
           component={Link}
           href="/home"
           className={"flex items-center h-12 hover:bg-neutral-50"}
-            onClick={toggle}
+          onClick={toggle}
         >
           <Center className="px-4">Home</Center>
         </UnstyledButton>
@@ -54,7 +60,7 @@ export function Shell({ children }: Props) {
           component={Link}
           href="/contact"
           className={"flex items-center h-12 hover:bg-neutral-50"}
-            onClick={toggle}
+          onClick={toggle}
         >
           <Center className="px-4">Contact</Center>
         </UnstyledButton>
@@ -62,13 +68,16 @@ export function Shell({ children }: Props) {
           component={Link}
           href="/about"
           className={"flex items-center h-12 hover:bg-neutral-50"}
-            onClick={toggle}
+          onClick={toggle}
         >
           <Center className="px-4">About</Center>
         </UnstyledButton>
+        {!user && <LoginButton className="mx-4" />}
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
+
+      <AppShell.Footer></AppShell.Footer>
     </AppShell>
   );
 }
